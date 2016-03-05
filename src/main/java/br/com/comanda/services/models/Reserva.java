c4d7ac955a94b7c8bea2c61154cc9cc0e284566c
vitorqshr <vitor.q.s.h.r@gmail.com>
2016-03-05 18:27:47 -0300

    reserva e sistema

---
+++ src/main/java/br/com/comanda/services/models/Reserva.java
@@ -0,0 +1,11 @@
+package br.com.comanda.services.models;
+
+import java.util.Date;
+
+public class Reserva {
+	private int idReserva, quantidadePessoa;
+	private Date dataReserva, dataCadastro, dataReservaFinal;
+	private double preco;
+	private String descricao;
+
+}
---
+++ src/main/java/br/com/comanda/services/models/Sistema.java
@@ -0,0 +1,7 @@
+package br.com.comanda.services.models;
+
+public class Sistema {
+	private int idSistema;
+	private String sistema;
+
+}
